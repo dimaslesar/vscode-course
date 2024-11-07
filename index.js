ef44e9d1903bfed62b551f454b567e85c2e11a60
Dmytro <dimaslesar>
2024-11-07 16:03:26 +0200

    '#' added <p> index.html

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const getUser = async () => {
   try {
-    const response = await fetch('https://vk.com')
-    const data = await response.json() 
+    const response = await fetch('https://vk.com');
+    const data = await response.json();
   } catch (e) {
     console.log(e);
   }
-}
-getUser()
- let num = 40;
+};
+getUser();
+
+let num = 40;
+const nums = 300000;
+let user = {};
