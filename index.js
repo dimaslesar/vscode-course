d5eb019d39dd3d82b4f7f0092bb64026fa8c68dc
Dmytro <dimaslesar>
2024-11-07 15:15:26 +0200

    '#' test

---
+++ index.js
@@ -0,0 +1,9 @@
+const getUser = async () => {
+  try {
+    const response = await fetch('https://vk.com')
+    const data = await response.json() 
+  } catch (e) {
+    console.log(e);
+  }
+}
+getUser()
